Now masking entire feed 20 after it produced NaNs in the normalization (not sure how). Also fixed a bug with a stored freqfilter parameter.

l2gen_filters.py
@@ -373,7 +373,7 @@ class Frequency_filter(Filter):
 
                 l2.tod[feed,sb] = l2.tod[feed,sb] - F.dot(a) - P.dot(m)
 
-                b = P
+                b = P.copy()
                 b[~np.isfinite(b)] = 0
                 b[:,0] /= np.linalg.norm(b[:,0])
                 b[:,1] /= np.linalg.norm(b[:,1])
@@ -514,7 +514,7 @@ class Masking(Filter):
         poly.run(l2_local)
         del(poly)
         l2_local.write_level2_data(name_extension=f"_mask_freqfilter")
-        print(f"[{rank}] [{self.name}] Finished local/masking polyfilter in {time.time()-t0:.1f} s.")
+        print(f"[{rank}] [{self.name}] Finished local/masking freqfilter in {time.time()-t0:.1f} s.")
         
         print(f"[{rank}] [{self.name}] Running local PCA filter for masking purposes...")
         t0 = time.time()

l2gen_l2class.py
@@ -45,13 +45,16 @@ class level2_file:
             self.Nsb = self.tod.shape[1]
             self.Nfreqs = self.tod.shape[2]
             self.Ntod = self.tod.shape[3]
+            self.corr_template = np.zeros((self.Nfeeds, self.Nsb*self.Nfreqs, self.Nsb*self.Nfreqs))  # The correlated induced by different filters, to be subtracted in masking.
 
             ### Preliminary Masking ###
             self.freqmask = np.ones((self.Nfeeds, self.Nsb, self.Nfreqs), dtype=bool)
             self.freqmask_reason = np.zeros_like(self.freqmask, dtype=int)
             self.freqmask_reason_string = []
             self.freqmask_counter = 0
-            self.corr_template = np.zeros((self.Nfeeds, self.Nsb*self.Nfreqs, self.Nsb*self.Nfreqs))  # The correlated induced by different filters, to be subtracted in masking.
+            self.freqmask[self.feeds==20] = False
+            self.freqmask_reason[self.feeds==20] += 2**self.freqmask_counter; self.freqmask_counter += 1
+            self.freqmask_reason_string.append("Feed 20")
             self.n_nans = np.sum(~np.isfinite(self.tod), axis=-1)
             self.freqmask[self.n_nans > 0] = False
             self.freqmask_reason[self.n_nans > 0] += 2**self.freqmask_counter; self.freqmask_counter += 1