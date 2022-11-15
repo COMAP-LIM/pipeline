6f466afb81cad98781205c0c8d422460160d91a3
Nils-Ole Stutzer <nilsole2009@live.no>
2022-11-15 14:09:05 +0100

    Adding some files for new python mapmaker

---
+++ tod2comap/tod2comap.py
@@ -0,0 +1,21 @@
+import argparse
+from typing import Dict, Any
+import h5py
+import numpy as np
+import numpy.typing as ntyping
+from COmap import COmap
+
+
+class TOD2COmap():
+    """COMAP mapmaker class
+    """
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def get_pointing_matrix(self):
+        pass
+
+    def get_
