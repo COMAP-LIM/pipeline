3c7415bf9cd08191cab17d6c11f9d309b2dea5b2
jgslunde <jonas.s.lunde@outlook.com>
2022-11-22 16:27:13 +0100

    started implementing lanczos. main part is working but I gave up finishing it

--- C_libs/PCA/PCAlib.cpp
+++ C_libs/PCA/PCAlib.cpp
@@ -70,9 +70,6 @@ void PCA(float *X, double *r, double *err, int n, int p, int max_iter, double er
         for(int col=0; col<p; col++){
             r[col] = s[col];
         }
-        cout << iter << " ";
-        cout << lambda << " ";
-        cout << err[iter] << endl;
         if(err[iter] < err_tol){
             for(int i=iter; i<max_iter; i++){
                 err[i] = 0.0;
@@ -81,6 +78,158 @@ void PCA(float *X, double *r, double *err, int n, int p, int max_iter, double er
         }
     }
 }
+
+
+extern "C"
+void eigensolver(double *X, double *eigvecs, int p, int max_iter, double err_tol){
+    double *s = new double[p];
+    double *r = new double[p];
+    double lambda = 0.0;
+
+    for(int eigvec_num=0; eigvec_num<p; eigvec_num++){
+
+        for(int i=0; i<p; i++){
+            r[i] = 1.0;
+        }
+        double norm = get_norm(r, p);
+        for(int i=0; i<p; i++){
+            r[i] /= norm;
+        }
+
+        for(int iter=0; iter<max_iter; iter++){
+            double err = 0.0;
+            for(int i=0; i<p; i++){
+                s[i] = 0.0;
+            }
+            for(int row=0; row<p; row++){
+                s[row] = dot_product(&X[row*p], r, p);
+            }
+
+            lambda = dot_product(r, s, p);
+            for(int col=0; col<p; col++){
+                err += fabs(r[col] - s[col]/lambda)/p;
+            }
+            normalize_vector(s, p);
+            for(int col=0; col<p; col++){
+                r[col] = s[col];
+            }
+            if(err < err_tol){
+                break;
+            }
+        }
+
+        for(int i=0; i<p; i++){
+            eigvecs[eigvec_num*p + i] = r[i];
+        }
+
+        for(int row=0; row<p; row++){
+            for(int col=0; col<p; col++){
+                X[row*p + col] -= lambda*r[row]*r[col];
+            }
+        }
+
+    }
+}
+
+
+extern "C"
+void PCA_lanczos(float *X, double *V, double *alphas, double *betas, int n, int p, int m){
+
+    double *w = new double[p];
+    double alpha = 0.0;
+    double beta = 0.0;
+
+    for(int row=0; row<n; row++){
+        double xv_dot = dot_product_float_double(&X[row*p], V, p);
+        for(int col=0; col<p; col++){
+            w[col] += X[row*p + col]*xv_dot;
+        }
+    }
+
+    alpha = dot_product(w, V, p);
+    alphas[0] = alpha;
+    for(int col=0; col<p; col++){
+        w[col] -= alpha*V[col];
+    }
+
+
+    for(int iter=1; iter<m; iter++){
+        beta = get_norm(w, p);
+        betas[iter-1] = beta;
+
+        for(int col=0; col<p; col++){
+            V[iter*p + col] = w[col]/beta;
+        }
+
+        for(int col=0; col<p; col++){
+            w[col] = 0.0;
+        }
+        for(int row=0; row<n; row++){
+            double xv_dot = dot_product_float_double(&X[row*p], &V[iter*p], p);
+            for(int col=0; col<p; col++){
+                w[col] += X[row*p + col]*xv_dot;
+            }
+        }
+
+        alpha = dot_product(w, &V[iter*p], p);
+        alphas[iter] = alpha;
+
+        for(int col=0; col<p; col++){
+            w[col] = w[col] - alpha*V[iter*p + col] - beta*V[(iter-1)*p + col];
+        }
+
+        // Double re-orthogonalization
+        for(int prev_iter=0; prev_iter<iter; prev_iter++){
+            double inner_prod = dot_product(&V[iter*p], &V[prev_iter*p], p);
+            for(int col=0; col<p; col++){
+                V[iter*p + col] -= inner_prod*V[prev_iter*p + col];
+            }
+        }
+        double norm = get_norm(&V[iter*p], p);
+        for(int col=0; col<p; col++){
+            V[iter*p + col] /= norm;
+        }
+
+        for(int prev_iter=0; prev_iter<iter; prev_iter++){
+            double inner_prod = dot_product(&V[iter*p], &V[prev_iter*p], p);
+            for(int col=0; col<p; col++){
+                V[iter*p + col] -= inner_prod*V[prev_iter*p + col];
+            }
+        }
+        norm = get_norm(&V[iter*p], p);
+        for(int col=0; col<p; col++){
+            V[iter*p + col] /= norm;
+        }
+
+
+        // Tried to implement the full calculate T and the eigenvectors thing, but gave up:
+        // if(iter >= 5){
+        //     double *T = new double[iter*iter];
+        //     double *T_eigvecs = new double[iter*iter];
+
+        //     for(int i=0; i<iter*iter; i++){
+        //         T[i] = 0.0;
+        //         T_eigvecs[i] = 0.0;
+        //     }
+
+        //     T[0] = alphas[0];
+        //     T[1] = betas[0];
+        //     for(int i=1; i<iter-1; i++){
+        //         T[iter*i+i-1] = betas[i-1];
+        //         T[iter*i+i] = alphas[i];
+        //         T[iter*i+i+1] = betas[i];
+        //     }
+
+        //     eigensolver(T, T_eigvecs, iter, 200, 1e-12);
+
+        // }
+
+
+    }
+
+
+}
+
 
 
 int main(){
