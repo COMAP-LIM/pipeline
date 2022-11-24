eacd90f1d945432cae72d1eb35b4502443d5290e
Nils-Ole Stutzer <nilsole2009@live.no>
2022-11-24 17:41:41 +0100

    Added more in map initializer

--- tod2comap/COmap.py
+++ tod2comap/COmap.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Optional
 import h5py
 import numpy as np
 import numpy.typing as ntyping
@@ -50,7 +51,7 @@ class COmap:
         self.keys = self._data.keys()
 
     def init_emtpy_map(
-        self, field_info: tuple, decimation_freqs: int, make_nhit: bool = False
+        self, field_info: tuple, decimation_freqs: tuple, make_nhit: bool = False
     ) -> None:
         """Methode used to set up empty maps and field grid for given field.
         Both an empty numerator and denominator map are generated to acumulate
@@ -69,6 +70,9 @@ class COmap:
         """
         GRID_SIZE, PIX_RES, FIELD_CENTER = field_info
 
+        # Number of pixels in {RA, DEC}
+        NSIDE_RA, NSIDE_DEC = GRID_SIZE
+
         # Number of feeds
         NFEED = 20
 
@@ -78,52 +82,92 @@ class COmap:
         if make_nhit:
             # Empty hit map
             self._data["nhit"] = np.zeros(
-                (NFEED, GRID_SIZE, GRID_SIZE, NSB * decimation_freqs), dtype=np.int32
+                (NFEED, NSIDE_RA, NSIDE_DEC, NSB * decimation_freqs), dtype=np.int32
             )
 
         # Empty denomitator map containing sum TOD / sigma^2
         self._data["numerator_map"] = np.zeros(
-            (NFEED, GRID_SIZE, GRID_SIZE, NSB * decimation_freqs), dtype=np.float32
+            (NFEED, NSIDE_RA, NSIDE_DEC, NSB * decimation_freqs), dtype=np.float32
         )
 
         # Empty denomitator map containing sum 1 / sigma^2
         self._data["denominator_map"] = np.zeros_like(self._data["numerator_map"])
 
         # RA/Dec grid
-        RA = np.zeros(GRID_SIZE)
-        DEC = np.zeros(GRID_SIZE)
+        RA_center = np.zeros(NSIDE_RA)
+        DEC_center = np.zeros(NSIDE_DEC)
+        RA_edge = np.zeros(NSIDE_RA + 1)
+        DEC_edge = np.zeros(NSIDE_DEC + 1)
+
         dRA = PIX_RES[0]
         dDEC = PIX_RES[1]
 
-        # Min values in RA/Dec. directions
-        if GRID_SIZE % 2 == 0:
-            RA_min = FIELD_CENTER[0] - dRA * GRID_SIZE / 2.0
-            DEC_min = FIELD_CENTER[1] - dDEC * GRID_SIZE / 2.0
+        # Min values in RA/Dec. directions (i.e. bin edges)
+        if NSIDE_RA % 2 == 0:
+            RA_min = FIELD_CENTER[0] - dRA * NSIDE_RA / 2.0
 
         else:
-            RA_min = FIELD_CENTER[0] - dRA * GRID_SIZE / 2.0 - dRA / 2.0
-            DEC_min = FIELD_CENTER[1] - dDEC * GRID_SIZE / 2.0 - dDEC / 2.0
+            RA_min = FIELD_CENTER[0] - dRA * NSIDE_RA / 2.0 - dRA / 2.0
 
-        # Defining piRAel centers
-        RA[0] = RA_min + dRA / 2
-        DEC[0] = DEC_min + dDEC / 2
+        if NSIDE_DEC % 2 == 0:
+            DEC_min = FIELD_CENTER[1] - dDEC * NSIDE_DEC / 2.0
+        else:
+            DEC_min = FIELD_CENTER[1] - dDEC * NSIDE_DEC / 2.0 - dDEC / 2.0
 
-        for i in range(1, GRID_SIZE):
-            RA[i] = RA[i - 1] + dRA
-            DEC[i] = DEC[i - 1] + dDEC
+        # Defining pixel centers
+        RA_center[0] = RA_min + dRA / 2
+        DEC_center[0] = DEC_min + dDEC / 2
+        for i in range(1, NSIDE_RA):
+            RA_center[i] = RA_center[i - 1] + dRA
+
+        for i in range(1, NSIDE_DEC):
+            DEC_center[i] = DEC_center[i - 1] + dDEC
+
+        # Defining pixel edges
+        RA_edge[0] = RA_min
+        DEC_edge[0] = DEC_min
+        for i in range(1, NSIDE_RA + 1):
+            RA_edge[i] = RA_edge[i - 1] + dRA
+
+        for i in range(1, NSIDE_DEC + 1):
+            DEC_edge[i] = DEC_edge[i - 1] + dDEC
 
         # Save grid in private data dict
-        self._data["ra_centers"] = RA
-        self._data["dec_centers"] = DEC
-        self.ra_min = RA[0]
-        self.dec_min = DEC[0]
+        self._data["ra_centers"] = RA_center
+        self._data["dec_centers"] = DEC_center
+        self._data["ra_edges"] = RA_edge
+        self._data["dec_edges"] = DEC_edge
 
-    def write_map(self, outpath: str) -> None:
+        # Save number of pixels
+        self._data["n_ra"] = NSIDE_RA
+        self._data["n_dec"] = NSIDE_DEC
+
+        # Save number of sidebands and channels
+        self._data["n_sidebands"] = NSB
+        self._data["n_channels"] = decimation_freqs
+
+        # Save patch center
+        self._data["patch_center"] = np.array(FIELD_CENTER)
+
+        # Minimum grid centers
+        self.ra_min = RA_center[0]
+        self.dec_min = DEC_center[0]
+
+        # Define keys of internal map data dictionary
+        self.keys = self._data.keys()
+
+        # Map starts out not being PCA subtracted
+        self._data["is_pca_subtr"] = False
+
+    def write_map(self, outpath: Optional[str] = None) -> None:
         """Method for writing map data to file.
 
         Args:
             outpath (str): path to save output map file.
         """
+
+        if not outpath:
+            outpath = self.path
 
         if self._data["is_pca_subtr"]:
             # If PCA subtraction was performed append number
@@ -141,7 +185,6 @@ class COmap:
             print("Saving map to: ", outpath)
             with h5py.File(outpath, "w") as outfile:
                 for key in self.keys:
-                    # print(key)
                     outfile.create_dataset(key, data=self._data[key])
 
     def __getitem__(self, key: str) -> ntyping.ArrayLike:
@@ -167,3 +210,12 @@ class COmap:
         self._data[key] = value
         # Get new keys
         self.keys = self._data.keys()
+
+    def __delitem__(self, key: str) -> None:
+        """Method for deleting value corresponding to key
+
+        Args:
+            key (str): Key to dataset
+        """
+        # Set new item
+        del self._data[key]
