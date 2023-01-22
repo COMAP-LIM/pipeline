a155bc5e1254b9f80b820d3b0bff7c185a85fb5e
Nils-Ole Stutzer <nilsole2009@live.no>
2023-01-22 15:59:19 +0100

    Forgot to move if block in COmap object

--- tod2comap/COmap.py
+++ tod2comap/COmap.py
@@ -15,6 +15,12 @@ from astropy import wcs
 from astropy.io import fits
 from astropy.cosmology import FlatLambdaCDM
 
+field_id_dict = {
+    "co2": "field1",
+    "co7": "field2",
+    "co3": "field3",
+}
+
 
 @dataclass
 class COmap:
@@ -213,17 +219,135 @@ class COmap:
         self._data["is_sim"] = False
         self._data["is_simcube"] = False
 
+    def get_hdu_list(self, map_key, wcs):
+        # Primary header for saving important general metadata
+        primary_header = fits.Header()
+        primary = fits.PrimaryHDU(header=primary_header)
+
+        field_name = os.path.basename(self.path).split("_")[0]  # i.e co2, co7 or co6
+        field_id = field_id_dict[field_name]  # i.e. field1 field2 or field3
+
+        primary_header["FIELDNM"] = field_name
+        primary_header["FIELDID"] = field_id
+        primary_header["ISPCASBR"] = self._data["is_pca_subtr"]
+        primary_header["ISCUBE"] = self._data["is_simcube"]
+        primary_header["ISSIM"] = self._data["is_sim"]
+
+        primary_header["PATCHDEC"] = self._data["patch_center"][0]
+        primary_header["PATCHRA"] = self._data["patch_center"][1]
+
+        if self.params is not None and "params" not in self.keys:
+            for idx, key in enumerate(
+                vars(self.params)
+            ):  # Writing entire parameter file to separate hdf5 group.
+                if getattr(self.params, key) == None:  # hdf5 didn't like the None type.
+                    param = "None"
+                else:
+                    param = str(getattr(self.params, key))
+
+                # Add parameter to metadata and use the parameter name (key) as comment since parameter names are too long to be name of the header entry.
+                primary_header[f"P{idx:07}"] = (param, key)
+
+        NFEED = 20  # Number of feeds
+        NSIDEBAND = self._data["n_sidebands"]
+        NCHANNEL = self._data["n_channels"]
+
+        NRA = self._data["n_ra"]
+        NDEC = self._data["n_dec"]
+
+        nhit_key = re.sub(r"map", "nhit", map_key)
+        sigma_wn_key = re.sub(r"map", "sigma_wn", map_key)
+
+        if "coadd" in map_key:
+            map_data = (
+                self._data[map_key].copy().reshape(NSIDEBAND * NCHANNEL, NDEC, NRA)
+            )
+
+            nhit_data = (
+                self._data[nhit_key].copy().reshape(NSIDEBAND * NCHANNEL, NDEC, NRA)
+            )
+
+            sigma_wn_data = (
+                self._data[sigma_wn_key].copy().reshape(NSIDEBAND * NCHANNEL, NDEC, NRA)
+            )
+        else:
+            map_data = (
+                self._data[map_key]
+                .copy()
+                .reshape(NFEED, NSIDEBAND * NCHANNEL, NDEC, NRA)
+            )
+
+            nhit_data = (
+                self._data[nhit_key]
+                .copy()
+                .reshape(NFEED, NSIDEBAND * NCHANNEL, NDEC, NRA)
+            )
+
+            sigma_wn_data = (
+                self._data[sigma_wn_key]
+                .copy()
+                .reshape(NFEED, NSIDEBAND * NCHANNEL, NDEC, NRA)
+            )
+
+        # Header from wcs_dict
+        map_header = wcs.to_header()
+        map_header["BTYPE"] = "Temperature"
+        map_header["BUNIT"] = "K"
+
+        map_hdu = fits.ImageHDU(map_data, header=map_header, name="MAP")
+
+        nhit_header = wcs.to_header()
+        nhit_header["BTYPE"] = "Number of hits"
+        nhit_hdu = fits.ImageHDU(nhit_data, header=nhit_header, name="NHIT")
+
+        sigma_wn_header = wcs.to_header()
+        sigma_wn_header["BTYPE"] = "Temperature"
+        sigma_wn_header["BUNIT"] = "K"
+        sigma_wn_hdu = fits.ImageHDU(
+            sigma_wn_data, header=sigma_wn_header, name="SIGMA_WN"
+        )
+
+        primary_header["HDU1"] = "MAP"
+        # HDU lists with primary (metadata) and one of map, hit map and white noise map
+        map_hdu_list = [
+            primary,
+            map_hdu,
+        ]
+
+        primary_header["HDU1"] = "NHIT"
+        nhit_hdu_list = [
+            primary,
+            nhit_hdu,
+        ]
+
+        primary_header["HDU1"] = "SIGMA_WN"
+        sigma_wn_hdu_list = [
+            primary,
+            sigma_wn_hdu,
+        ]
+
+        return map_hdu_list, nhit_hdu_list, sigma_wn_hdu_list
+
     def write_map(
         self,
         outpath: Optional[str] = None,
         primary_splits: Optional[list] = None,
         params: Optional[argparse.Namespace] = None,
+        save_hdf5=True,
+        save_fits=True,
     ) -> None:
         """Method for writing map data to file.
 
         Args:
             outpath (str): path to save output map file.
         """
+
+        self.params = params
+
+        if not save_hdf5 and not save_fits:
+            raise ValueError(
+                "Make sure to chose either to save map as HDF5 or FITS file(s)."
+            )
 
         if not outpath:
             outpath = self.path
@@ -250,56 +374,169 @@ class COmap:
             outpath = self.path[:-namelen]
             outpath += outname
 
-        if len(self._data) == 0:
-            raise ValueError("Cannot save map if data object is empty.")
-        else:
-            print("Saving map to: ", outpath)
-            with h5py.File(outpath, "w") as outfile:
-                outfile.create_group("wcs")
+        if save_fits:
+            wcs_coadd, wcs_full = self.get_full_wcs()
 
-                if primary_splits is not None:
-                    # Create all needed groups for multisplits
-                    outfile.create_group("multisplits")
+            outdir = os.path.dirname(outpath)
+            fits_dir = os.path.basename(outpath).split(".h5")[0]
 
-                    for primary_split in primary_splits:
-                        outfile.create_group(f"multisplits/{primary_split}")
+            outdir = os.path.join(outdir, fits_dir)
 
-                for key in self.keys:
-                    if "wcs" in key:
-                        # Saving World Coordinate System parameters to group
-                        for wcs_key, wcs_param in self._data["wcs"].items():
-                            outfile.create_dataset(f"wcs/{wcs_key}", data=wcs_param)
-                    elif "multisplits" in key:
-                        outfile.create_dataset(f"{key}", data=self._data[key])
+            # Make output directry if it does not exist
+            if not os.path.exists(outdir):
+                os.mkdir(outdir)
 
-                    elif "/" in key:
-                        # If splis are to be performed save data to correct group
-                        primary_split = key.split("_")[-1]
+            ######## Save full feed map ########
+            (
+                full_map_hdu_list,
+                full_nhit_hdu_list,
+                full_sigma_wn_hdu_list,
+            ) = self.get_hdu_list("map", wcs_full)
+            full_map_hdul = fits.HDUList(full_map_hdu_list)
+            full_nhit_hdul = fits.HDUList(full_nhit_hdu_list)
+            full_sigma_wn_hdul = fits.HDUList(full_sigma_wn_hdu_list)
 
-                        primary_split = primary_split[:4]
-                        outfile.create_dataset(
-                            f"multisplits/{primary_split}{key}", data=self._data[key]
-                        )
-                    elif "params" in key:
-                        print(len(self._data["params"].keys()))
-                        for param_key in self._data["params"].keys():
-                            # print(param_key)
-                            outfile[f"params/{param_key}"] = self._data["params"][
-                                param_key
-                            ]
-                    else:
-                        outfile.create_dataset(key, data=self._data[key])
+            full_map_outname = os.path.join(outdir, f"{fits_dir}_temperature.fits")
+            full_nhit_outname = os.path.join(outdir, f"{fits_dir}_nhit.fits")
+            full_sigma_wn_outname = os.path.join(outdir, f"{fits_dir}_sigma_wn.fits")
 
-                if params is not None and "params" not in self.keys:
-                    for key in vars(
-                        params
-                    ):  # Writing entire parameter file to separate hdf5 group.
-                        if (
-                            getattr(params, key) == None
-                        ):  # hdf5 didn't like the None type.
-                            outfile[f"params/{key}"] = "None"
+            print("Saving feed map to: ", full_map_outname)
+            print("Saving feed nhit to: ", full_nhit_outname)
+            print("Saving feed sigma_wn to: ", full_sigma_wn_outname)
+
+            full_map_hdul.writeto(full_map_outname, overwrite=True)
+            full_nhit_hdul.writeto(full_nhit_outname, overwrite=True)
+            full_sigma_wn_hdul.writeto(full_sigma_wn_outname, overwrite=True)
+
+            ######## Save feed-coadded map ########
+            (
+                coadd_map_hdu_list,
+                coadd_nhit_hdu_list,
+                coadd_sigma_wn_hdu_list,
+            ) = self.get_hdu_list("map_coadd", wcs_coadd)
+            coadd_map_hdul = fits.HDUList(coadd_map_hdu_list)
+            coadd_nhit_hdul = fits.HDUList(coadd_nhit_hdu_list)
+            coadd_sigma_wn_hdul = fits.HDUList(coadd_sigma_wn_hdu_list)
+
+            coadd_map_outname = os.path.join(
+                outdir, f"{fits_dir}_coadd_temperature.fits"
+            )
+            coadd_nhit_outname = os.path.join(outdir, f"{fits_dir}_coadd_nhit.fits")
+            coadd_sigma_wn_outname = os.path.join(
+                outdir, f"{fits_dir}_coadd_sigma_wn.fits"
+            )
+
+            print("Saving feed-coadded map to: ", coadd_map_outname)
+            print("Saving feed-coadded nhit to: ", coadd_nhit_outname)
+            print("Saving feed-coadded sigma_wn to: ", coadd_sigma_wn_outname)
+
+            coadd_map_hdul.writeto(coadd_map_outname, overwrite=True)
+            coadd_nhit_hdul.writeto(coadd_nhit_outname, overwrite=True)
+            coadd_sigma_wn_hdul.writeto(coadd_sigma_wn_outname, overwrite=True)
+
+            # Make sure split output directories exist, make them if not
+            if primary_splits is not None:
+                # Create all needed groups for multisplits
+                split_basedir = os.path.join(outdir, "multisplits")
+                # Make output directry if it does not exist
+                if not os.path.exists(split_basedir):
+                    os.mkdir(split_basedir)
+                for primary_split in primary_splits:
+                    split_dir = os.path.join(split_basedir, f"{primary_split}")
+                    if not os.path.exists(split_dir):
+                        os.mkdir(split_dir)
+
+                    ######## Save feed-coadded split map ########
+                    for key in self.keys:
+                        if primary_split in key and "map" in key:
+                            which_split = key.split("_")[
+                                1
+                            ]  # e.g. want "elev0ambt1" of key name "map_elev0ambt1"
+
+                            (
+                                split_map_hdu_list,
+                                split_nhit_hdu_list,
+                                split_sigma_wn_hdu_list,
+                            ) = self.get_hdu_list(key, wcs_full)
+                            split_map_hdul = fits.HDUList(split_map_hdu_list)
+                            split_nhit_hdul = fits.HDUList(split_nhit_hdu_list)
+                            split_sigma_wn_hdul = fits.HDUList(split_sigma_wn_hdu_list)
+
+                            split_map_outname = os.path.join(
+                                split_dir, f"{fits_dir}_{which_split}_temperature.fits"
+                            )
+                            split_nhit_outname = os.path.join(
+                                split_dir, f"{fits_dir}_{which_split}_nhit.fits"
+                            )
+                            split_sigma_wn_outname = os.path.join(
+                                split_dir, f"{fits_dir}_{which_split}_sigma_wn.fits"
+                            )
+
+                            print("Saving split map to: ", split_map_outname)
+                            print("Saving split nhit to: ", split_nhit_outname)
+                            print(
+                                "Saving split sigma_wn to: ",
+                                split_sigma_wn_outname,
+                            )
+
+                            split_map_hdul.writeto(split_map_outname, overwrite=True)
+                            split_nhit_hdul.writeto(split_nhit_outname, overwrite=True)
+                            split_sigma_wn_hdul.writeto(
+                                split_sigma_wn_outname, overwrite=True
+                            )
+
+        if save_hdf5:
+            if len(self._data) == 0:
+                raise ValueError("Cannot save map if data object is empty.")
+            else:
+                print("Saving map to: ", outpath)
+                with h5py.File(outpath, "w") as outfile:
+                    outfile.create_group("wcs")
+
+                    if primary_splits is not None:
+                        # Create all needed groups for multisplits
+                        outfile.create_group("multisplits")
+
+                        for primary_split in primary_splits:
+                            outfile.create_group(f"multisplits/{primary_split}")
+
+                    for key in self.keys:
+                        if "wcs" in key:
+                            # Saving World Coordinate System parameters to group
+                            for wcs_key, wcs_param in self._data["wcs"].items():
+                                outfile.create_dataset(f"wcs/{wcs_key}", data=wcs_param)
+                        elif "multisplits" in key:
+                            outfile.create_dataset(f"{key}", data=self._data[key])
+
+                        elif "/" in key:
+                            # If splis are to be performed save data to correct group
+                            primary_split = key.split("_")[-1]
+
+                            primary_split = primary_split[:4]
+                            outfile.create_dataset(
+                                f"multisplits/{primary_split}{key}",
+                                data=self._data[key],
+                            )
+                        elif "params" in key:
+                            print(len(self._data["params"].keys()))
+                            for param_key in self._data["params"].keys():
+                                # print(param_key)
+                                outfile[f"params/{param_key}"] = self._data["params"][
+                                    param_key
+                                ]
                         else:
-                            outfile[f"params/{key}"] = getattr(params, key)
+                            outfile.create_dataset(key, data=self._data[key])
+
+                    if params is not None and "params" not in self.keys:
+                        for key in vars(
+                            params
+                        ):  # Writing entire parameter file to separate hdf5 group.
+                            if (
+                                getattr(params, key) == None
+                            ):  # hdf5 didn't like the None type.
+                                outfile[f"params/{key}"] = "None"
+                            else:
+                                outfile[f"params/{key}"] = getattr(params, key)
 
     def __getitem__(self, key: str) -> npt.ArrayLike:
         """Method for indexing map data as dictionary
@@ -343,9 +580,9 @@ class COmap:
         """
         angular_wcs_params = self._data["wcs"]
 
-        NFEED, NSIDEBAND, NCHANNEL, _, _ = self._data[
-            "map"
-        ].shape  # Change this later when we have access to frequency array
+        NFEED = 20  # Number of feeds
+        NSIDEBAND = self._data["n_sidebands"]
+        NCHANNEL = self._data["n_channels"]
 
         wcs_dict_coadd = {
             "CTYPE1": angular_wcs_params["CTYPE1"],
