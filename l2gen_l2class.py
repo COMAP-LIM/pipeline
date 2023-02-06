Fixed a couple of bugs which appeared with the temporal masking.

l2gen_l2class.py
@@ -110,6 +110,14 @@ class level2_file:
 
     def write_level2_data(self, name_extension=""):
         self.acceptrate = np.mean(self.freqmask, axis=(-1))
+        self.sigma0 = np.zeros((self.Nfeeds, self.Nsb, self.Nfreqs))
+        self.chi2 = np.zeros((self.Nfeeds, self.Nsb, self.Nfreqs))
+        self.Ntod_effective = np.zeros((self.Nfeeds))
+        for ifeed in range(self.Nfeeds):
+            self.Ntod_effective[ifeed] = int(np.sum(self.mask_temporal[ifeed]))
+            tod_local = self.tod[ifeed][:,:,self.mask_temporal[ifeed]]
+            self.sigma0[ifeed] = np.std(tod_local[:,:,1:] - tod_local[:,:,:-1], axis=-1)/np.sqrt(2)
+            self.chi2[ifeed] = (np.sum(tod_local**2, axis=-1)/self.sigma0[ifeed]**2 - self.Ntod_effective[ifeed])/np.sqrt(2*self.Ntod_effective[ifeed])
         outpath = os.path.join(self.level2_dir, self.fieldname)
         if not os.path.exists(outpath):
             os.mkdir(outpath)
@@ -136,9 +144,9 @@ class level2_file:
             f["feature"] = self.feature_bit
             f["scanid"] = self.scanid
             f["obsid"] = self.obsid
-            f["sigma0"] = np.std(self.tod[:,:,:,1:] - self.tod[:,:,:,:-1], axis=-1)/np.sqrt(2)
+            f["sigma0"] = self.sigma0
             f["n_nan"] = self.n_nans
-            f["chi2"] = (np.sum(self.tod**2, axis=-1)/f["sigma0"][()]**2 - self.Ntod)/np.sqrt(2*self.Ntod)
+            f["chi2"] = self.chi2
             f["point_cel"] = np.zeros((self.Nfeeds, self.Ntod, 2))
             f["point_cel"][:,:,0] = self.ra
             f["point_cel"][:,:,1] = self.dec
@@ -146,7 +154,6 @@ class level2_file:
             f["point_tel"][:,:,0] = self.az
             f["point_tel"][:,:,1] = self.el
             f["decimation_time"] = 1
-
             f["is_sim"] = self.is_sim
             f["git_hash"] = self.git_hash
 