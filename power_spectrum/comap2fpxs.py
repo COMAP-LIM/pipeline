5624ce8d3b774924d11542061a4ebbd5896fd09b
Nils-Ole Stutzer <nilsole2009@live.no>
2023-03-24 19:56:10 +0100

    Define split map names in fpxs class

--- power_spectrum/comap2fpxs.py
+++ power_spectrum/comap2fpxs.py
@@ -2,17 +2,26 @@ from __future__ import annotations
 from typing import Optional
 import h5py
 import numpy as np
-import numpy.typing as npt
-from pixell import enmap
 from dataclasses import dataclass, field
-import re
 import os
-import argparse
+import sys
 import pickle
+import itertools
+
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(current)
+sys.path.append(parent_directory)
 
 
 class COMAP2FPXS():
     def __init__(self):
+        self.read_params()
+        self.read_cosmology()
+        self.read_jackknife_definition_file()
+        self.generate_split_map_names()
+
+    def run(self):
         return NotImplemented
 
     def read_params(self):
@@ -36,7 +45,7 @@ class COMAP2FPXS():
         self.accept_data_id_string = self.params.accept_data_id_string
 
         # Raising errors if required parameters are missing
-        if self.power_spectrum_dir in None:
+        if self.power_spectrum_dir is None:
             raise ValueError(
                 "A power spectrum data directory must be specified in parameter file or terminal."
             )
@@ -64,9 +73,125 @@ class COMAP2FPXS():
         Method that reads in the standard cosmology to use form pickled astropy object.
         """
         
-        cosmology_path = os.path.join(self.params.cosmology_path, self.params.cosmology_name)
+        cosmology_path = os.path.join(self.params.phy_cosmology_dir, self.params.phy_cosmology_name)
 
         with open(cosmology_path, mode="rb") as file:
             self.cosmology = pickle.load(file)
+
+    def read_jackknife_definition_file(self):
+        """Method that reads the jackknife/split definition file and outputs definition of split variables.
+        """
+
+        with open(self.params.jk_def_file, "r") as jk_file:
+            all_lines = jk_file.readlines()
+
+        # skip the first two lines (number of different jk and accr)
+        all_lines = all_lines[
+            2:
+        ]  
         
+        # Variables marked with "2" in jk_def_file. 
+        # The first split in heirarchy/split-tree
+        primary_variables = []  
+
+        # Variables marked with "3" in jk_def_file.
+        # The secondary successive splits in herarchy/split-tree
+        secondary_variables = []  
+
+        # The variable over which to perform cross-spectra. 
+        # Marked with "1" in jk_def_file
+        cross_variables = []  # extra 1
+
+        # List of all split variables
+        all_variables = []
+        
+        # Read and sort split variables to respective lists
+        for line in all_lines:
+            split_line = line.split()
+            variable = split_line[0]
+            number = split_line[1]
+
+            if len(split_line) > 2:
+                extra = split_line[2]
+            
+            if len(split_line) < 2 or len(split_line) == 2:
+                extra = "#"
+
+            all_variables.append(variable)
+
+            if number == "3":
+                secondary_variables.append(variable)
+
+            if number == "2":
+                primary_variables.append(variable)
+
+            if extra == "1":
+                cross_variables.append(variable)
+
+        # Find all feed-feed variables that are also primary variables or secondary variables
+        cross_and_primary = []
+        cross_and_secondary = []
+
+        for variable in all_variables:
+            if variable in primary_variables and variable in cross_variables:
+                cross_and_primary.append(variable)
+            if variable in cross_variables and variable in secondary_variables:
+                cross_and_secondary.append(variable)
+
+        
+        # Define as class attributes
+        self.primary_variables  = primary_variables 
+        self.secondary_variables  = secondary_variables 
+        self.cross_variables  = cross_variables 
+        self.all_variables  = all_variables 
+        self.cross_and_primary  = cross_and_primary 
+        self.cross_and_secondary  = cross_and_secondary 
+    
+
+
+    def generate_split_map_names(self):  
+        secondary_variables = self.secondary_variables
+        cross_and_primary = self.cross_and_primary
+        
+        split_map_combinations = []
+
+        # If some primary variables are simultaneously feed-feed variables
+        if (len(cross_and_primary) != 0):  
+            
+            number_of_secondary_variables = len(secondary_variables)  
+            
+            # Generate indices for all split combinations
+            combinations = list(itertools.product(range(self.params.split_base_number), repeat = number_of_secondary_variables))  
+            for primary_variable in cross_and_primary:
+
+                # Generating names of split combinations
+                for combo in combinations:
+                    # name = primary_variable + "/"
+                    name = ""
+                    for i, bin_number in enumerate(combo):
+                        name = name + f"{secondary_variables[i]}{bin_number}" 
+
+                    split_map_combinations.append(
+                        (f"multisplits/{primary_variable}/map_{primary_variable}{0}{name}",
+                         f"multisplits/{primary_variable}/map_{primary_variable}{1}{name}",)
+                    )
+
+        self.split_map_combinations = split_map_combinations
+
+
+
+
+        
+if __name__ == "__main__":
+    
+    comap2fpxs = COMAP2FPXS()
+    
+    print(comap2fpxs.primary_variables)
+    print(comap2fpxs.secondary_variables)
+    print(comap2fpxs.cross_variables)
+    print(comap2fpxs.all_variables)
+    print(comap2fpxs.cross_and_primary)
+    print(comap2fpxs.cross_and_secondary)
+    print(comap2fpxs.split_map_combinations)
+    
 
