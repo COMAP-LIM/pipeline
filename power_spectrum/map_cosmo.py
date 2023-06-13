151f71bd844a638781a44a66a0009b2940bcf8eb
Nils-Ole Stutzer <nilsole2009@live.no>
2023-06-13 15:36:07 +0200

    Remove redundant print and declare params a class attribute

--- power_spectrum/map_cosmo.py
+++ power_spectrum/map_cosmo.py
@@ -33,7 +33,6 @@ class MapCosmo:
         split: Optional[str] = None,
 
     ):
-        print("Init of map_cosmo")
         """Init method to set up needed class attributes.
 
         Args:
@@ -47,7 +46,7 @@ class MapCosmo:
             ValueError: If no feed is specified and split key is provided.
             ValueError: If split key does not contain 'map'.
         """
-
+        self.params = params
         if feed is not None:
             self.feed = feed + 1
         
