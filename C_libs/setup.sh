2f71d47edee474e2d3639cb797df69650af3e7d0
Nils-Ole Stutzer <nilsole2009@live.no>
2023-10-16 09:53:38 +0200

    Fixed minor typo

--- C_libs/setup.sh
+++ C_libs/setup.sh
@@ -1,6 +1,6 @@
 g++ -shared -std=c++11 -fopenmp -lpthread -fPIC -o norm/normlib.so.1 norm/normlib.cpp -lfftw3_omp -lfftw3 -lm -O3
 g++ -shared -std=c++11 -O3 -lm -lgsl -fopenmp -fPIC -o pointing/pointinglib.so.1 pointing/pointinglib.cpp
-g++ -shared -std=c++11 -O3 -lm -lgsl -fopenmp -fPIC -o polyfit/polyfit.so polyfit/polyfit.cpp
+g++ -shared -std=c++11 -O3 -lm -lgsl -fopenmp -fPIC -o polyfit/polyfit.so.1 polyfit/polyfit.cpp
 g++ -shared -lm -fPIC -o PCA/PCAlib.so.1 PCA/PCAlib.cpp -O3 -std=c++11 -fopenmp -lpthread
 g++ -shared tod2comap/mapbinner.cpp -o tod2comap/mapbinner.so.1 -fPIC -fopenmp -Ofast -std=c++11
 g++ -shared cube2tod/cube2tod.cpp -o cube2tod/cube2tod.so.1 -fPIC -fopenmp -Ofast -std=c++11
