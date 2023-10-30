8839b5776be0d2e1ea1d7946659a032301241406
jgslunde <jonas.s.lunde@outlook.com>
2023-10-30 15:13:56 +0100

    Another bug

--- l2gen_filters.py
+++ l2gen_filters.py
@@ -1760,6 +1760,9 @@ class Tsys_calc(Filter):
                 Thot = f[f"Thot"][()]
                 calib_times = f[f"calib_times"][()]
                 successful = f[f"successful"][()]
+            if l2.Nfreqs == 512:
+                Phot = np.mean(Phot.reshape((Phot.shape[0], 4, 512, 2, 2)), axis=-2)
+
         except Exception as e:
             Phot = np.zeros((l2.Nfeeds, l2.Nsb, l2.Nfreqs, 2)) + np.nan
             Thot = np.zeros((l2.Nfeeds, 2)) + np.nan
@@ -1768,9 +1771,6 @@ class Tsys_calc(Filter):
             l2.freqmask[:] = False
             l2.freqmask_reason[:] += 2**l2.freqmask_counter; l2.freqmask_counter += 1
             l2.freqmask_reason_string.append("Can't read or find Tsys calib file.")
-
-        if l2.Nfreqs == 512:
-            Phot = np.mean(Phot.reshape((Phot.shape[0], 4, 512, 2, 2)), axis=-2)
 
         n_cal = np.zeros(l2.Nfeeds, dtype=int)
         any_calib_failed = False
