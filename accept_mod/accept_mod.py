Fix of minor broadcast error

accept_mod/accept_mod.py
@@ -1617,7 +1617,7 @@ def implement_split(params, scan_data, jk_list, cutoff_list, string, n):
     elif string == 'dayn':
         # day/night split
         closetonight = extract_data_from_array(scan_data, 'night').copy()
-        closetonight += np.random.normal(0, 1e-10, closetonight.shape[0])
+        closetonight += np.random.normal(0, 1e-10, closetonight.shape[0])[:, None, None]
         cutoff = np.percentile(closetonight[accept_list], 50.0)
         jk_list[np.where(closetonight > cutoff)] += int(2 ** n)
         cutoff_list[n-1] = cutoff
@@ -1693,7 +1693,7 @@ def implement_split(params, scan_data, jk_list, cutoff_list, string, n):
     elif string == 'rain':
         # rain split 
         rain = extract_data_from_array(scan_data, 'rain').copy()
-        rain += np.random.normal(0, 1e-10, rain.shape[0])
+        rain += np.random.normal(0, 1e-10, rain.shape[0])[:, None, None]
         cutoff = np.percentile(rain[accept_list], 50.0)
         jk_list[np.where(rain > cutoff)] += int(2 ** n) 
         cutoff_list[n-1] = cutoff